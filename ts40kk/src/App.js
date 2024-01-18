8d3684224bf661a1aceb23554a9959b3d7289e9f
adfar-136 <adfarrasheed136@gmail.com>
2024-01-18 11:54:28 +0530

    done

--- ts40kk/src/App.js
+++ ts40kk/src/App.js
@@ -1,39 +1,15 @@
-import React, { useState } from 'react'
+import React, { useState,useEffect } from 'react'
 
-function App() {
-  // const [input,setInput] = useState("");
-  // const [password,setPassword] = useState("");
-  // const [email,setEmail] = useState("");
-
-  const [condition,setCondition]  =useState(false);
-  const [list,setList] = useState({
-    name:"",
-    email:"",
-    password:""
-  })
-  function handleSubmit(e){
-    e.preventDefault();
-    setCondition(true);
-  }
+export default function App() { 
+  const [ timer,setTimer] = useState(0);
+  useEffect(()=>{
+     setTimeout(()=>{
+      setTimer(new Date().toLocaleString())
+     },1000)
+  },[timer])
   return (
     <div>
-      <form onSubmit={handleSubmit}>
-        <input type="text"  value={list.name} onChange={(e)=>setList({name:e.target.value})}/>
-        <input type="password"  value={list.password} onChange={(e)=>{setList({password:e.target.value})}}/>
-        <input type="email" value={list.email} onChange={(e)=>setList({email:e.target.value})} />
-        <input type="submit" value="Submit" />
-        {condition && (
-          <>
-            <h1>Your Name is:{list.name}</h1>
-          <h1>Your Password is:{list.password}</h1>
-          <h1>Your Email is:{list.email}</h1>
-          </>
-        
-        )}
-        
-      </form>
+      {timer}
     </div>
   )
 }
-
-export default App
--- ts40kk/src/adfar.js
+++ ts40kk/src/adfar.js
@@ -7,3 +7,10 @@ var {name,age,gender} = obj
 console.log(name)
 console.log(age)
 console.log(gender)
+
+var arr1 = [1,2,3,4,5];
+var arr3 = [11,22,33,44,55];
+var arr2 = [...arr1,...arr3];
+arr1[3] = "a"
+console.log(arr2);
+console.log(arr1);
