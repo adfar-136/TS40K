aa11308602dc58a517e77913da6d7e6314f7ce1b
adfar-136 <adfarrasheed136@gmail.com>
2024-01-21 10:27:12 +0530

    done

--- ts40kk/src/components/NewsApp/News.js
+++ ts40kk/src/components/NewsApp/News.js
@@ -3,26 +3,85 @@ import Newsitem from './Newsitem'
 import "./News.css"
 export default function News() {
     const [data,setData] = useState([]);
-       
+    const [search,setSearch] = useState("")
+    const [page,setPage] = useState(1)
+    const [totalPage,setTotalPages] = useState(1);
+    const [condition,setCondition]= useState(true)
+    const pageSize = 9;
+    const fetchData = async()=>{
+        const data =await fetch(`https://newsapi.org/v2/everything?q=${search}&page=${page}&pageSize=${pageSize}&from=2023-12-20&sortBy=publishedAt&apiKey=af2eaad40b1940c7a82732643cef799a`)
+        const response = await data.json();
+        console.log(response)
+        console.log(response)
+        setData(response.articles);
+        setTotalPages(Math.ceil(100/pageSize))
+        
+       } 
+       console.log(totalPage) 
     useEffect(()=>{
-        const fetchData = async()=>{
-            const data =await fetch("https://newsapi.org/v2/everything?q=cricket&from=2023-12-19&sortBy=publishedAt&apiKey=28a2c6b3aa1945c2afd8012e42fdac9e")
-            const response = await data.json();
-            setData(response.articles)
-           }
+        if(page>1){
+            setCondition(false)
+        }
+        if(page === 1){
+            setCondition(true)
+        }
         fetchData();
-    },[])
+    },[page])
+    const handleSubmit =(e)=>{
+        e.preventDefault()
+      fetchData()
+    }
+    const handlePrev=()=>{
+        if (page>1){
+            setPage(page-1)
+        }
+    }
+    const handleNext=()=>{
+        if(page<totalPage){
+            setPage(page+1)
+        }
+    }
+    const handleChange=(pageNumber)=>{
+        setPage(pageNumber)
+    }
+    const buttenRendering=()=>{
+        const paginationButtons = [];
+        for(let i=1;i<=totalPage;i++){
+            paginationButtons.push(
+                <button onClick={()=>handleChange(i)}>{i}</button>
+            )
+        }
+        return paginationButtons;
+    }
   return (
     <>
+     <form className='form' onSubmit={handleSubmit}>
+        <input className='search' type="text" value={search} onChange={(e)=>{setSearch(e.target.value)}}/>
+        <input className='btnn' type="submit" value="Search" />
+       </form>
     <div className='main'>
-    
-        <ul className='container'>
+      
+        {data?(
+            <>
+            <ul className='container'>
             {data.map((item,index)=>(
                 <Newsitem key={index} title={item.title} description={item.description}
                 img ={item.urlToImage} url = {item.url}/>
             ))}
         </ul>
+        <div className="pagination">
+        <button onClick={handlePrev} disabled={condition}>Prev</button>
+        {buttenRendering()}
+        <button onClick={handleNext}>Next</button>
     </div>
+            </>
+            
+        ):(
+            <h1>Please enter a query to search news items</h1>
+        )}
+        
+    </div>
+    
     
     </>
     
--- ts40kk/src/components/NewsApp/Newsitem.js
+++ ts40kk/src/components/NewsApp/Newsitem.js
@@ -1,13 +1,15 @@
 import React from 'react'
-
+import "./News.css"
 export default function Newsitem({title,description,img,url}) {
   return (
     <div>
-        <div>
-            <h1>{title}</h1>
-            <p>{description}</p>
-            <img src={img} alt={title}/>
-            <a href={url} target='_blank' rel="noreferrer">Read More</a>
+        <div className='card'>
+            <h1 className='heading'>{title?title.slice(0,20):"Yahan Title nhi hain"}</h1>
+            <div className="inner">
+            <p className='para'>{description?description.slice(0,80):"Yahan Description Nhi Hain"}</p>
+            <img className="img" src={img?img:"https://img.etimg.com/thumb/msid-106949123,width-1200,height-630,imgsize-43518,overlay-economictimes/photo.jpg"} alt={title}/>
+            </div>
+            <a className='btn' href={url} target='_blank' rel="noreferrer">Read More</a>
         </div>
     </div>
   )
